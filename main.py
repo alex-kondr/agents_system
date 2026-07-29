31b8761967774a714ad7189cf3c6c8de42a64fae
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2026-07-29 13:38:01 +0300

    save

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
-from os import getenv
+import os
 import asyncio
 
-from aiogram import Bot, Dispatcher, Router
+from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from fastapi import FastAPI, Request
 
 from routers.agents import agent_router #Імпорт роутера логіки з товарами
 from routers.start import start_router #Імпорт роутера логіки start
@@ -13,26 +14,41 @@ from routers.start import start_router #Імпорт роутера логіки
 # Завантажимо дані середовища з файлу .env(За замовчуванням)
 load_dotenv()
 
+TOKEN = os.getenv("TELEGRAM_API")
+WEBHOOK_PATH = f"/bot/{TOKEN}"
+WEBHOOK_URL = os.getenv("RENDER_EXTERNAL_URL") + WEBHOOK_PATH  # Render сам надає RENDER_EXTERNAL_URL
+
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+app = FastAPI()
+
 
 # Усі обробники варто закріплювати за Router або Dispatcher
 root_router = Router()
 root_router.include_routers(agent_router) #Включення роутера в головний
 root_router.include_routers(start_router) #Включення роутера в головний
+dp.include_router(root_router)
+
+
+@app.on_event("startup")
+async def on_startup():
+    # Реєструємо Webhook у Telegram при запуску
+    await bot.set_webhook(url=WEBHOOK_URL)
+
+@app.post(WEBHOOK_PATH)
+async def bot_webhook(request: Request):
+    # Отримуємо оновлення від Telegram
+    update = types.Update.model_validate(await request.json(), context={"bot": bot})
+    await dp.feed_update(bot, update)
+    return {"status": "ok"}
 
 
 # Головна функція пакету
-async def main() -> None:
-   # Дістанемо токен бота з середовища
-   TOKEN = getenv("TELEGRAM_API")
-   # Створимо об'єкт Bot
-   bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-   dp = Dispatcher()
-   dp.include_router(root_router)
-   # Почнемо обробляти події для бота
-   await dp.start_polling(bot)
+# async def main() -> None:
+    # Почнемо обробляти події для бота
+    # await dp.start_polling(bot)
 
 
 # Точка входу
-if __name__ == "__main__":
-   asyncio.run(main())
+# if __name__ == "__main__":
+#    asyncio.run(main())
